259ae35360f0975728662d4cf33adf2d2f8fddb9
Paul Reynolds <usuallypaulreynolds@gmail.com>
2022-02-23 12:30:50 -0600

    Initial commit, Chapter 4 complete, about to add material from Chapter 5 Studio.

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,29 @@
+package restaurant;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    private ArrayList<MenuItem> selections;
+    private Date lastUpdated;
+
+    public ArrayList<MenuItem> getSelections() {
+        return selections;
+    }
+
+    public void setSelections(ArrayList<MenuItem> aSelections) {
+        selections = aSelections;
+    }
+
+    public void addSelection(MenuItem menuItem) {
+        selections.add(menuItem);
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date aLastUpdated) {
+        lastUpdated = aLastUpdated;
+    }
+}
